fc1c7379bdec60ad3b53996790b9dc72fa114d37
vskp007 <79186806+vskp007@users.noreply.github.com>
2021-02-17 09:54:20 +0300

    Delete start_deep.sh

--- start_deep.sh
+++
@@ -1,5 +0,0 @@
-#
-# Example shell file for starting PhoenixMiner to mine ETH
-#
-# IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-./deep/deep -pool us1.ethermine.org:4444 -wal 0x4144725b0FcD734fAF2Ca3b0641c82Da8fcCd727.worker -proto 3 -gt 7
